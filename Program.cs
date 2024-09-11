cc7699d2b3c5bab6c4e156c5539a8d6e4914723d
StrawmanDave <davidgb.strawman@gmail.com>
2024-09-11 08:41:13 -0700

    finished step: 6

--- Program.cs
+++ Program.cs
@@ -1,17 +1,5 @@
-﻿decimal min = decimal.MinValue;
-decimal max = decimal.MaxValue;
-Console.WriteLine($"The range of the decimal type is {min} and {max}");
-
-double a = 1.0;
-double b = 3.0;
-Console.WriteLine(a / b);
-
-decimal c = 1.0M;
-decimal d = 3.0M;
-Console.WriteLine(c / d);
-
-double x = Math.PI;
+﻿double x = Math.PI;
 double r = 2.50;
 double rTwo = r * r;
-
-Console.WriteLine(x * rTwo);
+double area = x * rTwo;
+Console.WriteLine(area);
--- obj/Debug/net8.0/lab-1-numeric-computation-in-c-StrawmanDave.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-1-numeric-computation-in-c-StrawmanDave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-1-numeric-computation-in-c-StrawmanDave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9734907e9b879b76dd5b702c03001f103ad5e0e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ab126fe67a4fb9025cfa2a457fa1cd8ca2e57b")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-1-numeric-computation-in-c-StrawmanDave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-1-numeric-computation-in-c-StrawmanDave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
